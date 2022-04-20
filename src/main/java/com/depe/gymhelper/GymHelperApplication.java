b268281b9fd5bc6030d7e7870c5ca97b9ece52fe
dawid <dawidp789@gmail.com>
2022-04-20 12:33:11 +0200

    initial commit

---
+++ src/main/java/com/depe/gymhelper/GymHelperApplication.java
@@ -0,0 +1,13 @@
+package com.depe.gymhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymHelperApplication.class, args);
+    }
+
+}
