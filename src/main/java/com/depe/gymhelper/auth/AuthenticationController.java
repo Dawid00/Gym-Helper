8d41b984e085887fb6d416cf7c5f28e7d53c6930
dawid <dawidp789@gmail.com>
2022-04-25 08:25:40 +0200

    move registerUser method to user controller

--- src/main/java/com/depe/gymhelper/auth/AuthenticationController.java
+++ src/main/java/com/depe/gymhelper/auth/AuthenticationController.java
@@ -1,8 +1,9 @@
 package com.depe.gymhelper.auth;
 
 
+
+
 import com.depe.gymhelper.user.UserDetailsServiceImpl;
-import com.depe.gymhelper.user.UserRegisterDto;
 import com.depe.gymhelper.user.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,24 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 class AuthenticationController {
 
-    private final UserService userService;
     private final TokenService tokenService;
     private final UserDetailsServiceImpl userDetailsService;
 
-    AuthenticationController(UserService userService, TokenService tokenService, UserDetailsServiceImpl userDetailsService) {
-        this.userService = userService;
+    AuthenticationController(final TokenService tokenService, final UserDetailsServiceImpl userDetailsService) {
         this.tokenService = tokenService;
         this.userDetailsService = userDetailsService;
-    }
-
-    @PostMapping
-    void registerUser(@RequestBody UserRegisterDto userRegisterDto){
-        userService.createUser(userRegisterDto);
     }
 
     @PostMapping("/login")
     ResponseEntity<AuthenticationResponseDto> loginUser(@RequestBody AuthenticationRequestDto authenticationRequestDto){
         return ResponseEntity.ok(new AuthenticationResponseDto(tokenService.createToken(userDetailsService.loadUserByUsername(authenticationRequestDto.getUsername()))));
     }
-
 }
