8e18eec297661c80b9fe6ddd67d645dbeeae51af
dawid <dawidp789@gmail.com>
2022-04-25 08:26:21 +0200

    add validation for AuthenticationRequestDto

--- src/main/java/com/depe/gymhelper/auth/AuthenticationRequestDto.java
+++ src/main/java/com/depe/gymhelper/auth/AuthenticationRequestDto.java
@@ -1,14 +1,22 @@
 package com.depe.gymhelper.auth;
 
-class AuthenticationRequestDto {
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class AuthenticationRequestDto {
+    @NotNull(message = "username is mandatory")
+    @NotBlank
+    @Length(min = 5, max = 25, message = "Username must have 5-25 characters")
     private String username;
+    @NotNull
     private String password;
 
     public String getUsername() {
         return username;
     }
 
-    void setUsername(String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
@@ -16,7 +24,7 @@ class AuthenticationRequestDto {
         return password;
     }
 
-    void setPassword(String password) {
+    public void setPassword(String password) {
         this.password = password;
     }
 }
