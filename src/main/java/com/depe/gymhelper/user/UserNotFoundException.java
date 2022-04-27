cbdb33e657e631a8e64bc084349bd6d075906898
dawid <dawidp789@gmail.com>
2022-04-27 20:26:25 +0200

    change message

--- src/main/java/com/depe/gymhelper/user/UserNotFoundException.java
+++ src/main/java/com/depe/gymhelper/user/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.depe.gymhelper.user;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String username) {
-        super("User with " + username + "not found");
+        super("User with " + username + " not found");
     }
 }
