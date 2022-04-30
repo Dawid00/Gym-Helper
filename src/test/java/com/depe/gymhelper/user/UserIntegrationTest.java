c0abaa021493d86b1306be62313cb1ce85c016d5
dawid <dawidp789@gmail.com>
2022-04-30 14:00:31 +0200

    add user integration test

--- src/test/java/com/depe/gymhelper/user/UserIntegrationTest.java
+++ src/test/java/com/depe/gymhelper/user/UserIntegrationTest.java
@@ -12,8 +12,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.depe.gymhelper.user.RoleType.ADMIN;
+import static com.depe.gymhelper.user.RoleType.USER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -22,6 +27,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @WithMockUser(username = "testUser", password = "test")
 class UserIntegrationTest {
 
+    @Autowired
+    private RoleRepository roleRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -32,17 +39,6 @@ class UserIntegrationTest {
     private AuthenticationUserService authenticationUserService;
     @Autowired
     private AuthenticationManager authenticationManager;
-
-
-    @Test
-    void shouldChangeEmail(){
-        var user = initDatabaseWithTestUser();
-        //when
-        underTest.changeEmail(new EmailRequest("new@gmail.com"));
-        //then
-        assertThat(user.getEmail()).isEqualTo("new@gmail.com");
-    }
-
 
     @ParameterizedTest
     @MethodSource("provideUsernames")
@@ -67,15 +63,187 @@ class UserIntegrationTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    void shouldChangeEmail(){
+        var user = initDatabaseWithTestUser();
+        //when
+        underTest.changeEmail(new EmailRequest("new@gmail.com"));
+        //then
+        assertThat(user.getEmail()).isEqualTo("new@gmail.com");
+    }
+
+    @Test
+    void shouldChangePassword(){
+        //when
+        var user = initDatabaseWithTestUser();
+        var oldPassword = user.getPassword();
+        underTest.changePassword(new PasswordRequest("newPassword"));
+        //then
+        assertThat(user.getPassword()).isNotEqualTo(oldPassword);
+    }
+
+    @Test
+    void shouldCreateUser(){
+        //given
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest();
+        registerUserRequest.setUsername("user123");
+        registerUserRequest.setPassword("password");
+        registerUserRequest.setEmail("user123@gmail.com");
+        registerUserRequest.setHeight(185L);
+        registerUserRequest.setWeight(75.0);
+        //when
+        Long id = underTest.createUser(registerUserRequest);
+        //then
+        assertThat(id).isNotNull();
+        assertThat(userRepository.findById(id).get().getEmail()).isEqualTo("user123@gmail.com");
+        assertThat(userRepository.findById(id).get().getUsername()).isEqualTo("user123");
+    }
+
+    @Test
+    void shouldDeleteUserById(){
+        //given
+        Long id = userRepository.save(new User(
+                "depe@gmail.com",
+                "depe123",
+                "password",
+                new AthleteInfo(185L, 85.0)
+        )).getId();
+        //when
+        underTest.deleteUser(id);
+        //then
+        assertThat(userRepository.findById(id)).isEqualTo(Optional.empty());
+        assertThat(userRepository.findAll()).hasSize(0);
+    }
+
+    @Test
+    void shouldDeleteLoggedUser(){
+        //given
+        initDatabaseWithTestUser();
+        //when
+        assertThat(userRepository.findAll()).hasSize(1);
+        underTest.deleteLoggedUser();
+        //then
+        assertThat(userRepository.findAll()).hasSize(0);
+    }
+
+    @Test
+    void shouldGiveAdminRoleToUser(){
+        //given
+        var user = initDatabaseWithTestUser();
+        //when
+        underTest.giveAdminRoleToUser(user.getUsername());
+        var result = user.getRoles();
+        var nameOfRole = result.stream()
+                .map(Role::getType)
+                .map(RoleType::getName)
+                .collect(Collectors.toSet());
+        //then
+        assertThat(result).hasSize(2);
+        assertThat(nameOfRole.containsAll(Set.of("ROLE_USER","ROLE_ADMIN"))).isTrue();
+    }
+
+    @Test
+    void shouldTakeAdminRoleToUser(){
+        //given
+        var user = initDatabaseWithTestUser();
+        user.addRole(roleRepository.findByType(ADMIN));
+        //when
+        underTest.takeAdminRoleFromUser(user.getUsername());
+        var result = user.getRoles();
+        var nameOfRole = result.stream()
+                .map(Role::getType)
+                .map(RoleType::getName)
+                .collect(Collectors.toSet());
+        //then
+        assertThat(result).hasSize(1);
+        assertThat(nameOfRole.contains("ROLE_USER")).isTrue();
+    }
+
+    @Test
+    void shouldGetQueryUserByUsername(){
+        //given
+        var expectedId = initDatabaseWithTestUser().getId();
+        //when
+        var result = underTest.getQueryUserByUsername("testUser");
+        //then
+        assertThat(result.getUsername()).isEqualTo("testUser");
+        assertThat(result.getId()).isEqualTo(expectedId);
+    }
+
+    @Test
+    void shouldGetAllUsers(){
+        var testUser = initDatabaseWithTestUser();
+        //when
+        var results = underTest.getAllUsers();
+        //then
+        assertThat(results).hasSize(1);
+        assertThat(results.contains(testUser)).isTrue();
+    }
+
+    @Test
+    void shouldUpdateUserByUsername(){
+        //given
+        initDatabaseWithTestUser();
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest();
+        registerUserRequest.setUsername("user123");
+        registerUserRequest.setPassword("password");
+        registerUserRequest.setEmail("user123@gmail.com");
+        registerUserRequest.setHeight(195L);
+        registerUserRequest.setWeight(89.0);
+        var user = userRepository.findAll().get(0);
+        //when
+        underTest.updateUserByUsername("testUser",registerUserRequest);
+        //then
+        assertThat(user.getUsername()).isEqualTo("user123");
+        assertThat(user.getAthleteInfo().getHeight()).isEqualTo(195L);
+        assertThat(user.getAthleteInfo().getWeight()).isEqualTo(89.0);
+    }
+
+    @Test
+    void shouldUpdateLoggedUser(){
+        //given
+        initDatabaseWithTestUser();
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest();
+        registerUserRequest.setUsername("user123");
+        registerUserRequest.setPassword("password");
+        registerUserRequest.setEmail("user123@gmail.com");
+        registerUserRequest.setHeight(195L);
+        registerUserRequest.setWeight(89.0);
+        var user = userRepository.findAll().get(0);
+        //when
+        underTest.updateLoggedUser(registerUserRequest);
+        //then
+        assertThat(user.getUsername()).isEqualTo("user123");
+        assertThat(user.getAthleteInfo().getHeight()).isEqualTo(195L);
+        assertThat(user.getAthleteInfo().getWeight()).isEqualTo(89.0);
+
+    }
+
+    @Test
+    void shouldDeleteUserByUsername(){
+        //given
+        Long id = userRepository.save(new User(
+                "depe@gmail.com",
+                "depe123",
+                "password",
+                new AthleteInfo(185L, 85.0)
+        )).getId();
+        //when
+        underTest.deleteUserByUsername("depe123");
+        //then
+        assertThat(userRepository.findById(id)).isEqualTo(Optional.empty());
+        assertThat(userRepository.findAll()).hasSize(0);
+    }
+
+
     private User initDatabaseWithTestUser(){
-        return userRepository.save(
-                new User(
-                        "testUser@gmail.com",
-                        "testUser",
-                        passwordEncoder.encode("test"),
-                        new AthleteInfo(185L, 75.0)
-                )
-        );
+        var user = new User(
+                "testUser@gmail.com",
+                "testUser",
+                passwordEncoder.encode("test"),
+                new AthleteInfo(185L, 75.0));
+        user.addRole(roleRepository.findByType(USER));
+        return userRepository.save(user);
     }
 
     private static Stream<Arguments> provideUsernames(){
@@ -91,6 +259,4 @@ class UserIntegrationTest {
                 Arguments.of("new@email.com",true)
         );
     }
-
-
 }
